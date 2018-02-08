81d75acf1913abf1456d91970166a8e0c336658a
Gowtham Garimella <ggarime1@jhu.edu>
2018-02-08 17:28:54 -0500

    Add rpyt controller class

--- include/gazebo_aerial_manipulation_plugin/rpytcontroller.h
+++ include/gazebo_aerial_manipulation_plugin/rpytcontroller.h
@@ -11,6 +11,7 @@ private:
   GazeboTime previous_sim_time_;///< Last simulation time
   double max_thrust_;///< Maximum thrust applied
   double min_thrust_;///< Minimum thrust applied
+  double max_dt_;///< Maximum time diff allowed
   // Body XYZ format
   gazebo::common::PID roll_pid_controller_;///< PID for roll
   gazebo::common::PID pitch_pid_controller_;///< PID for pitch
@@ -21,6 +22,7 @@ public:
     gazebo::math::Vector3 torque;
   };
 public:
-  RpytController(gazebo::math::Vector3 roll_pid_gains, gazebo::math::Vector3 pitch_pid_gains, gazebo::math::Vector3 yaw_pid_gains);
+  RpytController(gazebo::math::Vector3 roll_pid_gains, gazebo::math::Vector3 pitch_pid_gains, gazebo::math::Vector3 yaw_pid_gains,
+                 double max_torque, double max_thrust, double min_thrust=0.0);
   BodyWrench update(gazebo::math::Vector3 roll_pitch_yawrate, double thrust, GazeboTime sim_time);
 };
--- src/rpytcontroller.cpp
+++ src/rpytcontroller.cpp
@@ -2,28 +2,33 @@
 
 using namespace gazebo;
 
-RpytController::RpytController(math::Vector3 roll_pid_gains, math::Vector3 pitch_pid_gains, math::Vector3 yaw_pid_gains, double max_torque, double max_thrust, double min_thrust=0.0): previous_sim_time_(0.0)
-  , roll_pid_controller_(roll_pid_gains.x, roll_pid_gains.y, roll_pid_gains.z, max_torque/10.0, 0.0, max_torque, 0)
-  , pitch_pid_controller_(pitch_pid_gains.x, pitch_pid_gains.y, pitch_pid_gains.z, max_torque/10.0, 0.0, max_torque, 0)
-  , yaw_pid_controller_(yaw_pid_gains.x, yaw_pid_gains.y, yaw_pid_gains.z, max_torque/10.0, 0.0, max_torque, 0)
+RpytController::RpytController(math::Vector3 roll_pid_gains, math::Vector3 pitch_pid_gains, math::Vector3 yaw_pid_gains, double max_torque, double max_thrust, double min_thrust): previous_sim_time_(0.0)
+  , roll_pid_controller_(roll_pid_gains.x, roll_pid_gains.y, roll_pid_gains.z, max_torque/2.0, -max_torque/2.0, max_torque, -max_torque)
+  , pitch_pid_controller_(pitch_pid_gains.x, pitch_pid_gains.y, pitch_pid_gains.z, max_torque/2.0, -max_torque/2.0, max_torque, -max_torque)
+  , yaw_pid_controller_(yaw_pid_gains.x, yaw_pid_gains.y, yaw_pid_gains.z, max_torque/2.0, -max_torque/2.0, max_torque, -max_torque)
   , max_thrust_(max_thrust)
   , min_thrust_(min_thrust)
+  , max_dt_(1.0)
 {
 }
 
 RpytController::BodyWrench RpytController::update(math::Vector3 error_roll_pitch_yawrate, double thrust, common::Time sim_time) {
   RpytController::BodyWrench body_wrench;
   common::Time dt = sim_time - previous_sim_time_;
-  if(dt > 1.0) {
+  previous_sim_time_ = sim_time;
+  if(dt > max_dt_) {
     gzwarn<<"Time difference between previous and current sim time is larger than 1 second: "<<previous_sim_time_.Double()<<", "<<sim_time.Double()<<std::endl;
-    body_wrench.torque = math::Vector3::Zero();
+    body_wrench.torque = math::Vector3::Zero;
+    roll_pid_controller_.Reset();
+    pitch_pid_controller_.Reset();
+    yaw_pid_controller_.Reset();
   } else if(dt < 0) {
     gzwarn<<"Time difference between previous and current sim time is negative: "<<previous_sim_time_.Double()<<", "<<sim_time.Double()<<std::endl;
-    body_wrench.torque = math::Vector3::Zero();
+    body_wrench.torque = math::Vector3::Zero;
   } else {
-    body_wrench.torque.x = roll_pid_controller_.Update(error_roll_pitch_yawrate.x, sim_time);
-    body_wrench.torque.y = pitch_pid_controller_.Update(error_roll_pitch_yawrate.y, sim_time);
-    body_wrench.torque.z = yaw_pid_controller_.Update(error_roll_pitch_yawrate.z, sim_time);
+    body_wrench.torque.x = roll_pid_controller_.Update(error_roll_pitch_yawrate.x, dt);
+    body_wrench.torque.y = pitch_pid_controller_.Update(error_roll_pitch_yawrate.y, dt);
+    body_wrench.torque.z = yaw_pid_controller_.Update(error_roll_pitch_yawrate.z, dt);
   }
   body_wrench.force.x = body_wrench.force.y = 0;
   body_wrench.force.z = thrust>max_thrust_?max_thrust_:(thrust<min_thrust_)?min_thrust_:thrust;
